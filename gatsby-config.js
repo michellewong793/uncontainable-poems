0f2f3305c928dc2b3548c8ff87db68fbbe4dca2a
Michelle Wong <mwong46@smith.edu>
2018-08-20 08:59:23 -0400

    Added spongebob meme

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,7 +41,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId:'UA-124162286-1'
       },
     },
     `gatsby-plugin-feed`,
